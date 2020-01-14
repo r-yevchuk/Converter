c5512bd81dfc5b868523cb080e908783fc5c59bf
ryevc <r.yevchuk@gmail.com>
2020-01-14 17:55:23 +0200

    First commit

---
+++ app/src/main/java/ua/com/converter/MainActivity.java
@@ -0,0 +1,75 @@
+package ua.com.converter;
+
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.et_input);
+        editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(5)});
+
+        showResult(new Result());
+    }
+
+    public void startConvert(View v) {
+        String data = editText.getText().toString();
+        showResult(logic(data));
+    }
+
+    private void showResult(Result result) {
+        TextView tvResult = findViewById(R.id.tv_result);
+        tvResult.setText(result.toString());
+    }
+
+    private Result logic(String data) {
+        int min = 0;
+        int sec = 0;
+
+        if (data != null) {
+            float parsed = 0f;
+            try {
+                parsed = Float.parseFloat(data);
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Not a Number", Toast.LENGTH_SHORT).show();
+            }
+
+            float temp = 60 / parsed;
+            min = (int) temp;
+            sec = Math.round((temp - min) * 60);
+        }
+        return new Result(min, sec);
+    }
+
+    private class Result {
+        int min;
+        int sec;
+
+        Result() {
+            min = 0;
+            sec = 0;
+        }
+
+        Result(int min, int sec) {
+            this.min = min;
+            this.sec = sec;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "" + min + " m " + sec + " s";
+        }
+    }
+}
