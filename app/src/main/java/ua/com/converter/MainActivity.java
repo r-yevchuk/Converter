4ed9676c1fbc685bac49474db858bf20c48e37a0
Roman Yevchuk <r.yevchuk@gmail.com>
2020-05-19 16:00:50 +0300

    Appointing 'Enter' button to start convert

--- app/src/main/java/ua/com/converter/MainActivity.java
+++ app/src/main/java/ua/com/converter/MainActivity.java
@@ -2,16 +2,29 @@ package ua.com.converter;
 
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
+
+    private final View.OnKeyListener keyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                String data = editText.getText().toString();
+                showResult(logic(data));
+                return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.et_input);
         editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(5)});
+        editText.setOnKeyListener(keyListener);
 
         showResult(new Result());
     }
